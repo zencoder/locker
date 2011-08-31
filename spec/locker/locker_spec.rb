f7a1c1a5329650cf8caf9e59d1929df5016931d6
Nathan Sutton <nate@zencoder.com>
2011-08-30 23:09:20 -0700

    Add locker specs

--- spec/locker/locker_spec.rb
+++ spec/locker/locker_spec.rb
@@ -1,7 +1,119 @@
-require "locker"
+require "spec_helper"
 
 describe Locker do
-  before(:all) do
-    
+  before do
+    Lock.delete_all
+    FakeLock.fake_locks = {}
   end
+
+  describe "initialization" do
+    it "should have default values" do
+      locker = Locker.new("foo")
+      locker.key.should == "foo"
+      locker.renew_every.should == 10
+      locker.lock_for.should == 30
+      locker.model.should == Lock
+      locker.identifier.should match(/^#{Regexp.escape("host:#{Socket.gethostname} pid:#{Process.pid}")} guid:[a-f0-9]+$/)
+      locker.blocking.should be_false
+      locker.locked.should be_false
+    end
+
+    it "should have some overridable values" do
+      locker = Locker.new("bar", :renew_every => 20.seconds, :lock_for => 1.minute, :blocking => true, :model => FakeLock)
+      locker.key.should == "bar"
+      locker.renew_every.should == 20
+      locker.lock_for.should == 60
+      locker.model.should == FakeLock
+      locker.identifier.should match(/^#{Regexp.escape("host:#{Socket.gethostname} pid:#{Process.pid}")} guid:[a-f0-9]+$/)
+      locker.blocking.should be_true
+      locker.locked.should be_false
+    end
+
+    it "should ensure that the key exists" do
+      Lock.find_by_key("baz").should be_nil
+      Locker.new("baz", :renew_every => 20.seconds, :lock_for => 1.minute, :blocking => true)
+      Lock.find_by_key("baz").should_not be_nil
+      Locker.new("baz", :renew_every => 20.seconds, :lock_for => 1.minute, :blocking => true)
+    end
+
+    it "should validate renew_every and lock_for values" do
+      expect{ Locker.new("foo", :renew_every => 0) }.to raise_error(ArgumentError)
+      expect{ Locker.new("foo", :renew_every => 1) }.to_not raise_error
+      expect{ Locker.new("foo", :lock_for => 0) }.to raise_error(ArgumentError)
+      expect{ Locker.new("foo", :renew_every => 4, :lock_for => 2) }.to raise_error(ArgumentError)
+      expect{ Locker.new("foo", :renew_every => 1, :lock_for => 1.00001) }.to_not raise_error
+    end
+  end
+
+  describe "locking" do
+    it "should lock a record" do
+      locker = Locker.new("foo")
+      locker.get.should be_true
+      lock = Lock.find_by_key("foo")
+      lock.locked_until.should be <= (Time.now.utc + locker.lock_for)
+      lock.locked_by.should == locker.identifier
+      lock.locked_at.should be < Time.now.utc
+    end
+
+    it "should renew a lock" do
+      locker = Locker.new("foo")
+      locker.get.should be_true
+      lock = Lock.find_by_key("foo")
+      lock.locked_until.should be <= (Time.now.utc + locker.lock_for)
+      lock.locked_by.should == locker.identifier
+      lock.locked_at.should be < Time.now.utc
+      locker.renew.should be_true
+      lock = Lock.find_by_key("foo")
+      lock.locked_until.should be <= (Time.now.utc + locker.lock_for)
+      lock.locked_by.should == locker.identifier
+      lock.locked_at.should be < Time.now.utc
+    end
+
+    it "should raise when someone steals the lock" do
+      locker = Locker.new("foo")
+      locker.get.should be_true
+      lock = Lock.find_by_key("foo")
+      lock.update_attribute(:locked_by, "someone else")
+      expect{ locker.renew }.to raise_error(Locker::LockStolen)
+    end
+  end
+
+  describe "blocking" do
+    before do
+      @first_locker = Locker.new("foo", :renew_every => 0.2.second, :lock_for => 0.6.second)
+      @second_locker = Locker.new("foo")
+      @first_locker.get
+    end
+
+    it "should block and wait for the first lock to release before running the second" do
+      start_time = Time.now.to_f
+      @second_locker.run(true){"something innocuous"}
+      end_time = Time.now.to_f
+      time_ran = (end_time - start_time)
+      time_ran.should be >= 0.6, "Oops, time was #{end_time-start_time} seconds"
+    end
+  end
+
+  describe "non-blocking" do
+    before do
+      @first_locker = Locker.new("foo")
+      @second_locker = Locker.new("foo")
+      @first_locker.get
+    end
+
+    it "should return false when we can't obtain the lock" do
+      @second_locker.run{raise "SHOULD NOT RUN KTHXBAI"}.should be_false
+      @first_locker.run{ "something" }.should be_true
+    end
+
+    it "should take less than half a second to fail" do
+      start_time = Time.now.to_f
+      return_value = @second_locker.run{raise "SHOULD NOT RUN KTHXBAI"}
+      end_time = Time.now.to_f
+      return_value.should be_false
+      run_time = (end_time - start_time)
+      run_time.should be < 0.5
+    end
+  end
+
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,65 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+
 require 'rubygems'
 require 'bundler/setup'
+require 'active_record'
 
 require 'locker'
+
+ActiveRecord::Base.time_zone_aware_attributes = true
+ActiveRecord::Base.default_timezone = "UTC"
+
+config = YAML.load_file(File.join(File.dirname(__FILE__), 'database.yml'))
+ActiveRecord::Base.establish_connection(config.merge('database' => 'postgres', 'schema_search_path' => 'public'))
+begin
+  ActiveRecord::Base.establish_connection(config)
+rescue
+  ActiveRecord::Base.connection.create_database(config['database'], config.merge("encoding" => config['encoding'] || ENV['CHARSET'] || 'utf8'))
+  ActiveRecord::Base.establish_connection(config)
+end
+
+ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS locks")
+ActiveRecord::Base.connection.create_table(:locks) do |t|
+  t.string :locked_by
+  t.string :key
+  t.datetime :locked_at
+  t.datetime :locked_until
+end
+ActiveRecord::Base.connection.add_index :locks, :key, :unique => true
+
+class Lock < ActiveRecord::Base
+end
+
+class FakeLock
+  attr_accessor :locked_by, :key, :locked_at, :locked_until
+
+  cattr_accessor :fake_locks
+  self.fake_locks = {}
+
+  def self.find_by_key(key)
+    fake_locks[key]
+  end
+
+  def self.create(attributes={})
+    fake_locks[attributes[:key]] = new(attributes)
+    true
+  end
+
+  def initialize(attributes={})
+    attributes.each do |key, value|
+      send("#{key}=", value)
+    end
+  end
+end
+
+RSpec.configure do |c|
+  c.before do
+    ActiveRecord::Base.connection.increment_open_transactions
+    ActiveRecord::Base.connection.begin_db_transaction
+  end
+  c.after do
+    ActiveRecord::Base.connection.rollback_db_transaction
+    ActiveRecord::Base.connection.decrement_open_transactions
+  end
+end
