f613eddc8bc227dfc332ef1339564b9e01b67707
Nathan Sutton <nate@zencoder.com>
2011-08-30 23:24:12 -0700

    Make the generator work with older versions of ActiveRecord

--- lib/generators/locker/templates/migration.rb
+++ lib/generators/locker/templates/migration.rb
@@ -1,4 +1,5 @@
 class Create<%= plural_name.camelize %> < ActiveRecord::Migration
+  <%- if ActiveRecord::VERSION::MAJOR == 3 && ActiveRecord::VERSION::MINOR == 1 -%>
   def change
     create_table :<%= plural_name %> do |t|
       t.string :locked_by
@@ -9,4 +10,20 @@ class Create<%= plural_name.camelize %> < ActiveRecord::Migration
 
     add_index :<%= plural_name %>, :key, :unique => true
   end
+  <%- else -%>
+  def self.up
+    create_table :<%= plural_name %> do |t|
+      t.string :locked_by
+      t.string :key
+      t.datetime :locked_at
+      t.datetime :locked_until
+    end
+
+    add_index :<%= plural_name %>, :key, :unique => true
+  end
+
+  def self.down
+    drop_table :<%= plural_name %>
+  end
+  <%- end -%>
 end
