ddef2800747964ee07f6053f145dd663a5dbcf89
Nathan Sutton <nate@zencoder.com>
2012-12-25 02:01:36 -0600

    Require db_setup.rb

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,30 +7,7 @@ require 'active_record'
 
 require 'locker'
 
-ActiveRecord::Base.time_zone_aware_attributes = true
-ActiveRecord::Base.default_timezone = "UTC"
-
-config = YAML.load_file(File.join(File.dirname(__FILE__), 'database.yml'))
-begin
-  ActiveRecord::Base.establish_connection(config.merge('database' => 'postgres', 'schema_search_path' => 'public'))
-  ActiveRecord::Base.connection.create_database(config['database'], config.merge("encoding" => config['encoding'] || ENV['CHARSET'] || 'utf8'))
-rescue ActiveRecord::StatementInvalid => e
-  raise unless e.message =~ /database "locker_test" already exists/
-end
-
-ActiveRecord::Base.establish_connection(config)
-
-ActiveRecord::Base.connection.execute("DROP TABLE IF EXISTS locks")
-ActiveRecord::Base.connection.create_table(:locks) do |t|
-  t.string :locked_by
-  t.string :key
-  t.datetime :locked_at
-  t.datetime :locked_until
-end
-ActiveRecord::Base.connection.add_index :locks, :key, :unique => true
-
-class Lock < ActiveRecord::Base
-end
+require 'db_setup'
 
 class FakeLock
   attr_accessor :locked_by, :key, :locked_at, :locked_until
