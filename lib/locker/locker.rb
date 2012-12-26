6dc08113d3d37a35f5ebbf72c0b34dda2bdf8674
Nathan Sutton <nate@zencoder.com>
2012-12-26 14:40:02 -0600

    Add the ability to check if we have lost the lock

--- lib/locker/locker.rb
+++ lib/locker/locker.rb
@@ -5,19 +5,25 @@ class Locker
     SecureRandom = ActiveSupport::SecureRandom
   end
 
-  attr_accessor :identifier, :key, :model, :locked, :blocking
+  attr_accessor :identifier, :key, :model, :locked, :blocking, :check, :check_every
 
   class << self
     attr_accessor :model
   end
 
   def initialize(key, options={})
-    @identifier = "host:#{Socket.gethostname} pid:#{Process.pid} guid:#{SecureRandom.hex(16)}" rescue "pid:#{Process.pid} guid:#{SecureRandom.hex(16)}"
-    @key        = key
-    @model      = (options[:model] || self.class.model || ::Lock)
-    @blocking   = !!options[:blocking]
-    @locked     = false
-    @lock_id    = find_or_create_lock_record.id
+    @identifier  = "host:#{Socket.gethostname} pid:#{Process.pid} guid:#{SecureRandom.hex(16)}" rescue "pid:#{Process.pid} guid:#{SecureRandom.hex(16)}"
+    @key         = key
+    @model       = (options[:model] || self.class.model || ::Lock)
+    @blocking    = !!options[:blocking]
+    @locked      = false
+    @lock_id     = find_or_create_lock_record.id
+    @check       = options.fetch(:check, true)
+    @check_every = (options[:check_every] || 10.seconds).to_f
+
+    if @check
+      raise ArgumentError, "check_every must be greater than 0" if @check_every <= 0
+    end
   end
 
   def self.run(key, options={}, &block)
@@ -29,10 +35,30 @@ class Locker
     get(blocking)
 
     if @locked
-      block.call
-    end
+      if @check
+        begin
+          parent_thread = Thread.current
+          connection    = model.connection
 
-    @locked
+          checker = Thread.new do
+            while @locked
+              sleep @check_every
+              check(parent_thread, connection)
+            end
+          end
+
+          block.call
+        ensure
+          checker.exit rescue nil
+        end
+      else
+        block.call
+      end
+
+      true
+    else
+      false
+    end
   ensure
     release
   end
@@ -46,6 +72,15 @@ class Locker
     release_advisory_lock
   end
 
+  def check(thread=Thread.current, connection=model.connection)
+    if holds_advisory_lock?(connection)
+      true
+    else
+      thread.raise LockStolen
+      false
+    end
+  end
+
 
 private
 
@@ -56,7 +91,12 @@ private
   end
 
   def get_advisory_lock(blocking)
-    success = model.connection.select_value("SELECT pg_#{blocking ? "" : "try_"}advisory_lock(#{@lock_id})") == "t"
+    success = execute_lock == "t"
+
+    while !success && blocking
+      sleep 0.5
+      success = execute_lock == "t"
+    end
 
     if success
       update_all(["locked_by = ?, locked_at = clock_timestamp() at time zone 'UTC'", @identifier], ["id = ?", @lock_id])
@@ -65,13 +105,25 @@ private
     success
   end
 
+  def execute_lock
+    model.connection.select_value("SELECT pg_try_advisory_lock(#{@lock_id})")
+  end
+
   def release_advisory_lock
     update_all(["locked_by = NULL, locked_at = NULL"], ["id = ? AND locked_by = ?", @lock_id, @identifier])
-    model.connection.select_value("SELECT pg_advisory_unlock(#{@lock_id})") == "t"
+    execute_release == "t"
+  end
+
+  def execute_release
+    model.connection.select_value("SELECT pg_advisory_unlock(#{@lock_id})")
   end
 
   def update_all(*args)
     model.update_all(*args) > 0
+  end
+
+  def holds_advisory_lock?(connection)
+    "t" == connection.select_value("SELECT 't' FROM pg_locks WHERE locktype = 'advisory' AND pid = pg_backend_pid() AND objid = #{@lock_id}")
   end
 
 end
